ca5013d5d84b804d401950286f2be03505362591
sabbirz <sabbir.kuasha@gmail.com>
2022-09-09 15:50:36 +0600

    get and post method using controller with validation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products',function(){
-    return 'products page';
-});
+Route::get('/products',[ProductController::class, 'index']);
+
+Route::post('/products',[ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
