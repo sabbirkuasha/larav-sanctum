bc7efc57af1bcbc4a8b4639dc3f83f9426a532b7
Sabbirz <sabbir.kuasha@gmail.com>
2022-09-17 18:19:48 +0600

    sanctum protected route

--- routes/api.php
+++ routes/api.php
@@ -15,15 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 // Resource route
-Route::resource('products', ProductController::class);
+// Route::resource('products', ProductController::class);
 
 // Search API
-Route::get('/products/search/{name}',[ProductController::class, 'search']);
+// Route::get('/products/search/{name}',[ProductController::class, 'search']);
 
 // old code
-// Route::get('/products',[ProductController::class, 'index']);
-// Route::post('/products',[ProductController::class, 'store']);
-// Route::get('/products/{id}',[ProductController::class, 'show']);
+Route::get('/products',[ProductController::class, 'index']);
+Route::get('/products/{id}',[ProductController::class, 'show']);
+Route::get('/products/search/{name}',[ProductController::class, 'search']);
+
+//Protected Route
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
+});
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
